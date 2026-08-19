270566a217f947939e1fe5fce032eb8c126fc970
Abhishek <abhishek@learning.com>
2026-08-19 14:08:18 +0530

    Configured CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.database.database import Base, engine
@@ -19,6 +20,17 @@ app = FastAPI(
     version=settings.APP_VERSION
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
