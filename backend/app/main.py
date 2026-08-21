b76425723d1322048d1e04401f06c5f89ce332a2
Abhishek <abhishek@learning.com>
2026-08-21 20:59:57 +0530

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,6 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://inventory-and-order-management-syst-pi.vercel.app",
-        #https://inventory-and-order-management-syst-pi.vercel.app/
     ],
     allow_credentials=True,
     allow_methods=["*"],
